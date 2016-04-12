1e35acccdbfcc2d158f3bf3006fcc9e921929a02
el coruco <el.coruco@gmail.com>
2016-04-12 10:47:12 -0500

    hola mundo 3

--- public/test.php
+++ public/test.php
@@ -1,1 +1,14 @@
-<?php echo "hola mundo PHP"; ?>
+<?php
+$servername = "www14.df.gob.mx";
+$username = "DBc4biertos";
+$password = "contr4tos4b!ertos.2016*";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
