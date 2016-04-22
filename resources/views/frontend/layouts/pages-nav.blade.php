79390021a56df6720f899877a7ce43f2985441dc
hugovom <hugo.om@gmail.com>
2016-04-22 11:15:17 -0500

    fix #7

--- resources/views/frontend/layouts/pages-nav.blade.php
+++ resources/views/frontend/layouts/pages-nav.blade.php
@@ -1,12 +1,12 @@
 <!-- navigation -->
 <nav class="pages-nav">
 	<div class="pages-nav__item"><a class="link link--page" href="{{ url('home2') }}">Inicio</a></div>
-	<div class="pages-nav__item"><a class="link link--page" href="{{ url('v2') }}">Contratos</a></div>
+	<div class="pages-nav__item"><a class="link link--page" href="{{ url('contratos') }}">Contratos</a></div>
 	<div class="pages-nav__item"><a class="link link--page" href="{{ url('dependencia/1') }}">Dependencias</a></div>
 	<div class="pages-nav__item"><a class="link link--page" href="{{ url('proveedores') }}">Proveedores</a></div>
-	<div class="pages-nav__item"><a class="link link--page" href="{{ url('que-son') }}">¿Qué son las contrataciones abiertas?</a></div>
+	<div class="pages-nav__item"><a class="link link--page" href="{{ url('por-que') }}">¿Por qué las contrataciones abiertas?</a></div>
 	<div class="pages-nav__item"><a class="link link--page" href="{{ url('datos-abiertos') }}">Usar los datos</a></div>
-	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('glosario') }}">Glosario</a></div>
+	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('glosario') }}">Términos Clave</a></div>
 	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('contacto') }}">Contacto</a></div>
 	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('privacidad') }}">Privacidad &amp; Términos</a></div>
 </nav>
