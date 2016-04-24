a3cf61ba6b583273b5522aabf79d79b612823ef7
hugovom <hugo.om@gmail.com>
2016-04-23 19:09:55 -0500

    fix #12

--- resources/views/frontend/what.blade.php
+++ resources/views/frontend/what.blade.php
@@ -12,7 +12,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 col-xs-10 col-sm-offset-0 col-xs-offset-1">
-				<h1><span>En el gobierno</span> de la CD<span class="mx">MX</span> 
+				<h1><span>En el gobierno</span> de la <span>CD</span><span class="mx">MX</span> 
 					<span>estamos comprometidos</span> con la <span>transparencia y queremos que conozcas</span>
 					<strong>cómo gastamos tu dinero.</strong>
 				</h1>
