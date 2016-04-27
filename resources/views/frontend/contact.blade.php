f5d3b721effe547ae4d44230cdeb485dd72c611c
hugovom <hugo.om@gmail.com>
2016-04-27 14:15:08 -0500

    fix #28

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -15,7 +15,7 @@
 				<div class="codigo">
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
-							<p>Envía un correo a <a href="mailto:contratacionesabiertas@cdmx.gob.mx">contratacionesabiertas@cdmx.gob.mx</a>
+							<p>En caso de dudas, quejas o sugerencias, envía un correo a  <a href="mailto:contratacionesabiertas@cdmx.gob.mx">contratacionesabiertas@cdmx.gob.mx</a>
 							</p>
 						</div>
 					</div>
