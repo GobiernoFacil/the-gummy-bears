d365f1a46241c93d19acfae7758fe207629f7158
hugovom <hugo.om@gmail.com>
2016-04-27 18:37:15 -0500

    fix #29

--- resources/views/frontend/terms.blade.php
+++ resources/views/frontend/terms.blade.php
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-10 col-sm-offset-1">
-				<h1>Privacidad & Términos de uso del Portal de Contrataciones Abiertas</h1>
+				<h1>Términos y Condiciones de Uso del Portal de Contrataciones Abiertas</h1>
 			</div>
 		</div>
 		
@@ -15,7 +15,69 @@
 				<div class="codigo">
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
-						<!--proceso-->
+
+<h2>Sobre la página</h2>
+
+<p>Este sitio web fue diseñado por el Gobierno de la CDMX y tienen por objeto regular la forma y términos para la utilización del sistema electrónico de información pública gubernamental, denominado CONTRATACIONES ABIERTAS, por parte de los sujetos a que se refieren los artículos 1 de la Ley de Adquisiciones para el Distrito Federal y 1 de la Ley de Obras Públicas del Distrito Federal, así como de los licitantes, proveedores y contratistas, de conformidad con lo previsto en dichos ordenamientos legales.  </p>
+
+<p>Asimismo tiene la finalidad de transparentar los contratos públicos que celebra como entidad administrativa con terceros y, consecuentemente, poner a disposición del público en general la información relativa al uso del gasto público.</p>
+
+<p>El sitio fue liberado en abril de 2016 en su versión 1.0 y se encuentra bajo la responsabilidad de la Secretaría de Finanzas de la Ciudad de México. </p>
+
+<h2>Sobre el Gobierno de la Ciudad de México</h2>
+
+<p>El Gobierno de la Ciudad de México se encuentra conformado por todas las entidades de la Administración Pública Federal de la Ciudad de México, el cual se encuentra comprometido con la transparencia y la apertura de datos. </p>
+
+<p>Lo anterior se realiza en cumplimiento a lo dispuesto en la  Ley para hacer de la Ciudad de México una ciudad más abierta (Gaceta Oficial del Distrito Federal el 7 de octubre de 2015),  la Ley de gobierno electrónico del Distrito Federal (Gaceta Oficial del Distrito Federal el 7 de octubre de 2015) y atendiendo a los principios de legalidad, certeza jurídica, imparcialidad, información, celeridad, veracidad, transparencia y máxima publicidad.</p>
+
+<h2>Licencia</h2>
+
+<p>Todos los documentos y datos de la presente plataforma se otorgan bajo la Licencia de Gobierno Abierto de la Ciudad de México (LGACDMX), por lo que al hacer uso de los datos proporcionados usted acepta usarlos bajo dicho licenciamiento, mismo que bajo ninguna circunstancia se entenderá como exclusiva.</p>
+
+<p>En los términos de la LGACDMX usted puede:</p>
+<ul class="lalist">
+<li>Usar los datos para fines públicos o privados.</li>
+<li>Copiar, modificar, hacer trabajos derivados, publicar, adaptar, traducir y cualquier otra actividad que implique la transformación de datos a cualquier formato.</li>
+<li>Compartir y distribuir los documentos y datos por cualquier medio electrónico o físico con terceros. </li>
+<li>Explotar los datos y documentos en cualquier forma, incluso con carácter comercial.					 </li>
+</ul>
+
+<p>Sin embargo, en la misma licencia le obliga a:</p>
+<ul class="lalist">
+<li>Atribuir el origen de los datos al Gobierno de la Ciudad de México y, cuando sea posible, integrar un enlace a la licencia.</li>
+</ul>
+
+<p>Usted deberá abstenerse de</p>
+<ul class="lalist">
+	<li>Falsear la información o manipularla maliciosamente, de manera que muestra hechos erróneos o contrarios a lo que los datos por sí mismo demuestran.</li>
+	<li>Usarlos para transgredir cualquier ley nacional, internacional o derechos de terceros.</li>
+</ul>
+
+<p>La presente licencia no se extiende sobre posibles derechos intelectuales de terceros que accidentalmente pudieran estar contenidos en los documentos y datos proporcionados en la presente página tales como marcas, patentes, modelos de utilidad, diseños, secretos industriales o creaciones intelectuales que cuenten con registro nacional o internacional. </p>
+
+<h2>Datos personales</h2>
+
+<p>En los términos de la Ley de Transparencia y Acceso a la Información Pública del Distrito Federal, toda información generada, administrada o en posesión de los Entes Obligados se considera un bien de dominio público, accesible a cualquier persona. No obstante, en caso de que alguno de los documentos o datos presentados en este portal contengan datos considerados reservados o sensibles conforme a la Ley de Protección de Datos Personales, los mismos gozarán de la protección que otorga dicha ley y sus titulares podrán hacer uso de los derechos conferidos en dicha ley.</p>
+
+<h2>Política de privacidad</h2>
+
+<p>En este sitio no se recolecta información personal de ninguna clase para acceder a la misma. En el caso de los formularios de quejas y solicitudes que se encuentren en la presente página los mismos se colectan solamente para fines de seguimiento a la solicitud y se resguardan en archivo digital el término de un año tras la solicitud sólo para fines informativos, tales datos no se comparten con terceros ni se difunden de ninguna manera.</p>
+
+<p>Ni se utilizan cookies u otros medios para determinar la localización de los usuarios ni se instala software que extraiga información de los usuarios y las usuarias. Los metadatos de los usuarios se utilizarán sólo para fines estadísticos.</p>
+
+<h2>Condiciones de uso de la API de Contrataciones Abiertas CDMX</h2>
+
+<p>A través de esta página se pone a disposición del público la Interfaz de programación de aplicaciones (API, por sus siglas en inglés). El uso de la API también se encuentra sujeto a los términos de la Licencia de Gobierno Abierto de la Ciudad de México (LGACDMX) y su uso jamás deberá ceñirse a los siguientes criterios:</p>
+<ul class="lalist">
+<li>El usuario podrá usar la API de Contrataciones Abiertas CDMX para el desarrollo y funcionamiento de aplicaciones públicas o privadas de manera gratuita, abierta y sin otras restricciones que las explicadas en este apartado.</li>
+<li>Las aplicaciones que se desarrollen o funcionen a partir de la API jamás deberán ir en contra de alguna ley nacional, internacional o transgredir derechos de terceros.</li>
+<li>El usuario no deberá intentar ingresar o conectarse al sistema donde se encuentra alojado al API por otros medios que los que aquí se proporciona ni realizará ninguna acción que implique el menoscabo de la API, sus servidores o de cualquiera de los sistemas digitales de la Ciudad de México.</li>
+<li>Las aplicaciones derivadas a partir del uso de la API son únicamente responsabilidad de quien la desarrolla y la distribuye. Las responsabilidad y condiciones de contratación con el usuario final serán establecidas por el desarrollador sin ninguna responsabilidad jurídica o comercial para el Gobierno de la Ciudad de México.</li>
+<li>El uso de la API no es exclusivo para ninguna persona, por lo que la Ciudad de México no se hace responsable de aplicaciones que se desarrollen a partir de ella que dupliquen o imiten las funciones de la aplicación de otro usuario en ejercicio de la libre competencia.</li>
+<li>El uso de la API no hace al Gobierno de la Ciudad de México cotitular de ningún derecho de propiedad intelectual sobre la aplicación que funcione o se desarrolle a partir de su uso.</li>
+</ul>
+
+
 													
 						
 						</div>
