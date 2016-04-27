7c71f4804c1dc1b57358b87fddff3a151c4f95d1
hugovom <hugo.om@gmail.com>
2016-04-27 18:16:40 -0500

    fix #13

--- resources/views/frontend/what_includes/win.blade.php
+++ resources/views/frontend/what_includes/win.blade.php
@@ -1,37 +1,9 @@
 <div class="col-sm-10 col-sm-offset-1">
-	<h1>Al implementar el estándar internacional de <strong>contrataciones abiertas</strong>, el gobierno pone a tu disposición información sobre cuánto gasta, en qué gasta y a quiénes les compra.</h1>
+	<h1>Con la participación de <span>servidores públicos, empresarios, ciudadanos y academia</span> podremos fomentar la <strong>transparencia, rendición de cuentas</strong> y <strong>análisis del gasto público</strong> para una  mejor toma de decisiones.</h1>
+
+<div class="allthem">
+		<?php echo file_get_contents("img/svg/personajes_escenario.svg"); ?>
 </div>
-<div class="col-sm-4">
-	<div class="forthewin">
-		<?php echo file_get_contents("img/servidor_publico.svg"); ?>
-	</div>
-	<h3>Servidores Públicos</h3>
-	<ul>
-		<li>Desarrollo y manejo de cultura de legalidad</li>
-		<li>Rendición de cuentas</li>
-		<li>Mayor cercanía con ciudadanos</li>
-	</ul>
-</div>
-<div class="col-sm-4">
-	<div class="forthewin">
-		<?php echo file_get_contents("img/empresario.svg"); ?>
-	</div>
-	<h3>Empresarios</h3>
-	<ul>
-		<li>Mejora de competitividad</li>
-		<li>Transparencia en procesos de licitaciones públicas</li>
-	</ul>
-</div>
-<div class="col-sm-4">
-	<div class="forthewin">
-		<?php echo file_get_contents("img/ciudadanos.svg"); ?>
-	</div>
-	<h3>Ciudadanos</h3>
-	<ul>
-		<li>Cercanía con procesos de contrataciones</li>
-		<li>Mayor cercanía con tu gobierno</li>
-		<li>Conocer destino de recursos públicos</li>
-	</ul>
 </div>
 <div class="col-sm-12 quote">
 	<p>“El Modelo de Contrataciones Abiertas impulsa la competencia y consolida la integridad pública, generando transparencia y mejorando las condiciones para la contratación de productos y servicios.”<br> <strong>Dr. Miguel Ángel Mancera Espinosa, <span>Jefe de Gobierno</span></strong></p>
