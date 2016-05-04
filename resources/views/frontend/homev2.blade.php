596a2eee39b9134a7865701dcc006e9f46fbe785
hugovom <hugo.om@gmail.com>
2016-05-04 11:53:19 -0500

    remove commented code

--- resources/views/frontend/homev2.blade.php
+++ resources/views/frontend/homev2.blade.php
@@ -48,42 +48,6 @@
 			<div class="col-sm-10 col-sm-offset-1">
 				<iframe width="854px" height="480px" src="https://www.youtube.com/embed/pvI4QfhpEAo" frameborder="0" allowfullscreen></iframe>
 			</div>
-			<?php /*
-			<div class="col-sm-6">
-				<h1>La <span>CD</span><strong>MX</strong> es la <span>primer ciudad del mundo</span> en implementar el 
-					<a href="http://standard.open-contracting.org/?lang=es">Estándar de Contrataciones Abiertas</a></h1>
-				
-				<div id="mini-description-a">Con el <strong>Estándar de Contrataciones Abiertas</strong>, ahora los contratos públicos se liberarán desde la etapa de planeación hasta su ejecución, permitiéndote dar seguimiento al gasto de fondos públicos y mejorar la prestación de servicios.</div>
-
-				<!-- stage nav-->
-				@include ("frontend/home_slider/stages_nav")
-					<a href="{{ url('que-son') }}" class="btn">¿Qué son las Contrataciones Abiertas?</a>
-					<a href="{{ url('glosario') }}" class="btn">Consulta el Glosario para conocer más sobre Contrataciones Abiertas</a>
-
-			</div>
-			<div class="col-sm-6 pasos">
-				<!-- planeación -->
-				<div class="slide planeacion" data-step="planeacion">
-					@include ("frontend/home_slider/stages-planeacion")
-				</div>
-				<!--licitación-->
-				<div class="slide licitacion hide" data-step="licitacion">
-					@include ("frontend/home_slider/stages-licitacion")
-				</div>
-				<!--adjudicacion-->
-				<div class="slide adjudicacion hide">
-					@include ("frontend/home_slider/stages-adjudicacion")
-				</div>
-				<!--contrato-->
-				<div class="slide contrato hide">
-					@include ("frontend/home_slider/stages-contrato")
-				</div>
-				<!--implementación-->
-				<div class="slide implementacion hide">
-					@include ("frontend/home_slider/stages-implementacion")
-				</div>
-			</div>
-			*/?>
 			<div class="col-sm-4 col-sm-offset-2">
 				<a href="{{ url('por-que') }}" class="btn">¿Por qué las Contrataciones Abiertas?</a>
 			</div>
