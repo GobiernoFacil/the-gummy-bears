deb77dada6cd61cbe7c2c4b0fb512615b9f1e1ac
hugovom <hugo.om@gmail.com>
2016-05-31 18:20:36 -0500

    fix #49

--- resources/views/frontend/contracts/includes/planning.blade.php
+++ resources/views/frontend/contracts/includes/planning.blade.php
@@ -18,9 +18,19 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm-6">
               <p class="title_section">COMPRADOR</p>
               <p><a href="{{ url('dependencia/'. $elcontrato->buyer->id ) }}">{{ !empty($elcontrato->buyer->name) ? $elcontrato->buyer->name : ''}}</a></p>
+            </div>
+            <div class="col-sm-6">
+              <p class="title_section">DOCUMENTOS</p>
+              @if ($elcontrato->planning->documents)
+                <ol>
+	              	@foreach($elcontrato->planning->documents as $doc)
+					<li><a href="{{$doc->url}}">{{$doc->title}}</a> {{$doc->date}}</li>
+	              	@endforeach
+              	</ol>
+              @endif
             </div>
           </div>
         </div>
