f6ed836108ef7cbe8f4c2690ad3ff069cb54d335
hugovom <hugo.om@gmail.com>
2016-06-07 18:37:58 -0500

    fix #61

--- resources/views/frontend/contracts/contracts_list.blade.php
+++ resources/views/frontend/contracts/contracts_list.blade.php
@@ -92,7 +92,7 @@
 <div id="all-list-contracts" class="container">
 	<div class="row">
 		<div class="col-sm-9">
-			<h2 id="title_select_type" class="title_section select">Lista de <strong>Contrataciones Abiertas</strong></h2>
+			<h2 id="title_select_type" class="title_section select">Lista de <strong>Contrataciones Abiertas</strong> ({{$contracts->count()}})</h2>
 		</div>
 		<div class="col-sm-3">
 			<form class="select_type">
