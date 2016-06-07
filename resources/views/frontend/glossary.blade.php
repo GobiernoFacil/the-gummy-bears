8b55fc177f1b5713c0b63d109744950ad9b29891
hugovom <hugo.om@gmail.com>
2016-06-07 18:20:22 -0500

    fix #52

--- resources/views/frontend/glossary.blade.php
+++ resources/views/frontend/glossary.blade.php
@@ -6,7 +6,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-10 col-sm-offset-1">
-				<h1>Términos Clave - Portal Contrataciones Abiertas</h1>
+				<h1>Términos Clave del Portal de Contrataciones Abiertas</h1>
 			</div>
 		</div>
 		
@@ -17,7 +17,7 @@
 						<div class="col-sm-10 col-sm-offset-1">
 							<!--gobierno abierto-->
 							<h2>Gobierno Abierto</h2>
-							<p>Aquél que colabora con sus ciudadanos para aprovechar la inteligencia de diferentes sectores de la sociedad para tomar mejores decisiones en los procesos de diseño, elaboración, implementación y evaluación de políticas públicas, servicios públicos y programas gubernamentales, de forma abierta y transparente. Según la <a href="http://www.aldf.gob.mx/archivo-1b52054447d78a831c329f25931f03a5.pdf">Ley de para hacer de la Ciudad de México una Ciudad más Abierta</a>, para esto se tienen 8 principios básicos: 
+							<p>Aquél que colabora con sus ciudadanos para aprovechar las capacidades y conocimiento de diferentes sectores de la sociedad para mejorar la toma de decisiones en los procesos de diseño, elaboración, implementación y evaluación de políticas públicas, servicios públicos y programas gubernamentales, de forma abierta y transparente. De acuerdo con la <a href="http://www.aldf.gob.mx/archivo-1b52054447d78a831c329f25931f03a5.pdf">Ley de para hacer de la Ciudad de México una Ciudad más Abierta</a>, para lograr esto se tienen ocho principios básicos: 
 </p>
 							<ol>
 								<li><strong>Transparencia:</strong> Garantizar que las acciones y decisiones sobre el uso de los recursos públicos sea del conocimiento de la población de manera accesible, y que promueva su reutilización y redistribución.</li>
@@ -30,13 +30,13 @@
 								<li><strong>Retroalimentación:</strong> Para promover la mejora e innovación en las acciones del Gobierno se debe promover el intercambio de conocimiento e información entre las personas y las instancias del propio Gobierno. </li>
 
 							</ol>
-							<p>Otra ley que habla sobre este tema es la <a href="http://www.infodf.org.mx/documentospdf/Ley%20de%20Transparencia,%20Acceso%20a%20la%20Informaci%C3%B3n%20P%C3%BAblica%20y%20Rendici%C3%B3n%20de%20Cuentas%20de%20la%20Ciudad%20de%20M%C3%A9xico.pdf">Ley de Transparencia, Acceso a la Información Pública y Rendición de Cuentas de la Ciudad de México</a>.</p>
+							<p>Dentro de la misma línea, existe también la <a href="http://www.infodf.org.mx/documentospdf/Ley%20de%20Transparencia,%20Acceso%20a%20la%20Informaci%C3%B3n%20P%C3%BAblica%20y%20Rendici%C3%B3n%20de%20Cuentas%20de%20la%20Ciudad%20de%20M%C3%A9xico.pdf">Ley de Transparencia, Acceso a la Información Pública y Rendición de Cuentas de la Ciudad de México</a> que fomenta y defiende el derecho de todo ciudadano a ser informado y refuerza la obligación de las autoridades a proporcionar dicha información.</p>
 						
 							<div class="divider"></div>
 							
 							<!--datos abiertos gubernamentales-->
 							<h2>Datos abiertos gubernamentales</h2>
-							<p>Son los datos que se incluyen en el contrato, siempre y cuando no estén protegidos por la <a href="http://www.aldf.gob.mx/archivo-f73bdb295c017416ad640607e8aa1275.pdf">Ley de Protección de Datos Personales</a>.</p>
+							<p>Son los datos que se incluyen en el contrato, siempre y cuando no estén protegidos por la <a href="http://www.aldf.gob.mx/archivo-f73bdb295c017416ad640607e8aa1275.pdf">Ley de Protección de Datos Personales del Distrito Federal</a>.</p>
 
 							<div class="divider w"></div>
 							
@@ -59,12 +59,16 @@
 							<div class="divider"></div>
 							
 							<!--Proceso de Contratación-->
-							<h2>Proceso de Contratación</h2>
-							<p>Procedimiento administrativo por el que el Gobierno adquiere o renta bienes y servicios. Una contratación puede suceder por alguna de estas tres maneras:</p>
+							<h2>Proceso para contratar un bien o servicio</h2>
+							<p>Procedimiento administrativo por el que el Gobierno adquiere o renta bienes y servicios. Los métodos de contratación o adquisición según la Ley, son los siguientes:</p>
 							<ul>
-								<li><strong>Licitación pública:</strong> Se publica una convocatoria publicada en la <a href="http://www.consejeria.df.gob.mx/index.php/gaceta">Gaceta Oficial de la Ciudad de México</a> para una contratación de algún servicio, adquisición o arrendamiento, para que libremente los proveedores presenten propuestas a fin de asegurar al Gobierno las mejores condiciones en cuanto a precio, calidad, financiamiento, oportunidad, crecimiento económico, generación de empleo.</li>
-								<li><strong>Invitación restringida a cuando menos tres proveedores:</strong> Cuando la licitación pública no sea idónea para asegurar a la Administración Pública de la CDMX las mejores condiciones disponibles, se podrán contratar adquisiciones, arrendamientos y servicios a través de un procedimiento de invitación a cuando menos tres proveedores. En este caso no se publica en la Gaceta Oficial ni en la página de la Institución, sino que se emiten invitaciones a las personas físicas o morales que se estiman por medio de un estudio de mercado tengan los precios más bajos tomando en consideración la calidad y cantidad de los bienes o servicios a contratar. </li>
-								<li><strong>Adjudicación directa:</strong> En caso de no presentarse el mínimo de proposiciones señaladas en un procedimiento de invitación a cuando menos tres personas y se haya declarado desierto el procedimiento de licitación, el titular del área responsable de la contratación en la dependencia o entidad podrá adjudicar directamente el contrato siempre que no se modifiquen los requisitos establecidos en dichas invitaciones o en las bases de licitación. La <a href="http://www.aldf.gob.mx/archivo-b23bfec910d4dd4b6b1f1fff8ab0e453.pdf">Ley de Adquisiciones para el Distrito Federal</a> en el artículo 54 marca los casos en los que se aplica esta opción.</li>
+								<li><strong>Licitación pública (Abierta):</strong> Se publica una convocatoria en la <a href="http://www.consejeria.df.gob.mx/index.php/gaceta">Gaceta Oficial de la Ciudad de México</a> y otros medios electrónicos según el caso para una contratación de algún servicio, adquisición o arrendamiento, para que libremente los proveedores presenten propuestas con el fin de asegurar al Gobierno las mejores condiciones en el aspecto administrativo, legal, técnico y económico.</li>
+								<li><strong>Invitación restringida a cuando menos tres proveedores (Limitada):</strong> Cuando la licitación pública no sea idónea para asegurar a la Administración Pública de la CDMX las mejores condiciones disponibles, se podrán contratar adquisiciones, arrendamientos y servicios a través de un procedimiento de invitación a cuando menos tres proveedores. En este caso no se publica en la Gaceta Oficial ni en la página de la Institución, sino que se emiten invitaciones a las personas físicas o morales que se estiman por medio de un estudio de mercado tengan los precios más bajos tomando en consideración la calidad y cantidad de los bienes o servicios a contratar. </li>
+								<li><strong>Adjudicación directa (Limitada):</strong> En caso de no presentarse el mínimo de proposiciones señaladas en un procedimiento de invitación a cuando menos tres personas y se haya declarado desierto el procedimiento de licitación, el titular del área responsable de la contratación en la dependencia o entidad podrá adjudicar directamente el contrato siempre que no se modifiquen los requisitos establecidos en dichas invitaciones o en las bases de licitación. Esto se encuentra descrito en la Ley de Adquisiciones para el Distrito Federal en el artículo 55.</li>
+								<li><strong>Convenio de colaboración (Selectiva):</strong>Existen instituciones de Gobierno que ya ofrecen el servicio que se está buscando y por ello se debe acudir a ésta. Esto lo establece el artículo primero de la Ley de Adquisiciones para el Distrito Federal. Un ejemplo de esto podría ser la Comisión Mexicana de Impresión (COMISA) o la Policía Bancaria e Industrial (PBI).</li>
+								<li><strong>Adjudicación directa selectiva (Selectivo):</strong> Esta opción establece que hay proveedores que por tema de marca, patente o derechos de autor, solamente ellos pueden ofrecer el servicio o bien y por lo tanto es necesario recurrir a ellos. Ésta y cualquier otra consideración fuera de una licitación pública, las contempla el artículo 54 en sus diversas fracciones de la Ley de Adquisiciones.
+
+								</li>
 							</ul>
 							<p>De acuerdo con la Ley de Adquisiciones para el Distrito Federal, todo proceso de contratación debe ser analizado y aprobado por el Comité de Autorizaciones de Adquisiciones, Arrendamientos y Prestación de Servicios de la Administración Pública del Distrito Federal.</p>
 
@@ -75,9 +79,35 @@
 							<p>Para más información puedes consultar la <a href="http://www.aldf.gob.mx/archivo-b23bfec910d4dd4b6b1f1fff8ab0e453.pdf">Ley de Adquisiciones para el Distrito Federal</a> y el <a href="http://www.consejeria.cdmx.gob.mx/images/leyes/reglamentos/REGLAMENTODELALEYDEADQUISICIONESPARAELDISTRITOFEDERALII.pdf">Reglamento de la Ley de Adquisiciones para el Distrito Federal</a>.</p>
 							<div class="divider"></div>
 						
+							<!--CRITERIOS DE ADJUDICACIÓN-->
+							<h2>Criterios de Adjudicación</h2>
+							<p>Este es un proceso de análisis mediante el cual se determinará la adjudicación del bien o servicio que deben cumplir con las mejores condiciones para el Gobierno en el aspecto administrativo, legal, técnico y económico. La CDMX determina mediante dos criterios:</p>
+							<ol>
+								<li><strong>Mejor valor para el gobierno:</strong> Este término implica que posterior al análisis una propuesta sobresale en cuanto a que oferta las mejores condiciones en los cuatro aspectos anteriormente mencionados.</li>
+								<li><strong>Solo hay un participante:</strong> En este rubro recaen aquellas opciones que se contienen en el artículo 1o, 54 y 55 de la Ley de Adquisiciones. Es decir, adjudicación directa, adjudicación directa limitada y convenio de colaboración.</li>
+							</ol>
+							<div class="divider"></div>
+							
+							<!--SUFICIENCIA PRESUPUESTAL-->
+							<h2>Suficiencia Presupuestal</h2>
+							<p>Provisión de recursos para cubrir los requerimientos de remuneración de servicios personales para la adquisición de bienes y la contratación de servicios. Dictado por el Artículo 28 de la Ley de Adquisiciones.</p>
+							<div class="divider"></div>
+							
+							<!--TIPOS DE CONTRATOS SEGÚN EL MONTO ADJUDICADO-->
+							<h2>Tipos de Contratos según el monto adjudicado</h2>
+							<p>Te encontrarás con  varios formatos de contratos en este portal y esto se debe a que el tipo de contrato se rige por el monto adjudicado, todos tienen la misma formalidad y están aprobados por la Consejería Jurídica y de Servicios Legales (CEJUR). Estos se encuentran en el punto 4.7 de la <a href="http://www.om.cdmx.gob.mx/circularuno_unobis/circularunoyunobis.pdf">Circular 1</a> publicada en la Gaceta Oficial.</p>
+							<ol>
+							<li><strong>Facturas debidamente requisitada:</strong> Este formato se utiliza cuando el monto a contratar es menor a 50 mil pesos.</li>
+							<li><strong>Contrato Pedido:</strong> Son aquellos contratos realizados por montos entre 50,001 pesos hasta 200,000 pesos.</li>
+							<li><strong>Contrato Tipo:</strong> Este se efectúa cuando el monto rebasa los 200,001 pesos.</li>
+							<li><strong>Contrato Multianual:</strong> Puede tener cualquiera de los formatos anteriores por el monto, sin embargo este se caracteriza por el tiempo en el que se ejerce ya que abarca más de un ejercicio fiscal.</li>
+							</ol>
+							<div class="divider"></div>
+							
 							<!--proceso-->
 							<h2>Archivos .json</h2>
 							<p>Formato para el intercambio de datos, alternativa al formato XML. Una de sus ventajas es que puede ser leído por cualquier lenguaje de programación. Si descargas estos archivos podrás ver a detalle en qué fase va una contratación, así como sus documentos correspondientes.</p>
+							<p>Para más información sobre los términos que usa el Estándar dentro de los archivos .json puedes descargar este documento: <a href="{{ url('archivos/CatalogosElementosEstandarContratacionesAbiertas-CDMX.pdf')}}">Catálogo de elementos.pdf</a></p>
 													
 							<div class="divider w"></div>							
 							
