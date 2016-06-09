b810c1b8772e47f2bfb9c9bf0b4ee0b871ad2e08
hugovom <hugo.om@gmail.com>
2016-06-08 23:48:49 -0500

    fix #58

--- resources/views/frontend/what_includes/win.blade.php
+++ resources/views/frontend/what_includes/win.blade.php
@@ -1,9 +1,9 @@
 <div class="col-sm-10 col-sm-offset-1">
 	<h1>Con la participación de <span>servidores públicos, empresarios, ciudadanos y academia</span> podremos fomentar la <strong>transparencia, rendición de cuentas</strong> y <strong>análisis del gasto público</strong> para una  mejor toma de decisiones.</h1>
-
+<?php /*
 <div class="allthem">
-		<?php echo file_get_contents("img/svg/personajes_escenario.svg"); ?>
-</div>
+		 echo file_get_contents("img/svg/personajes_escenario.svg"); 
+</div> */ ?>
 </div>
 <div class="col-sm-12 quote">
 	<p>“El Modelo de Contrataciones Abiertas impulsa la competencia y consolida la integridad pública, generando transparencia y mejorando las condiciones para la contratación de productos y servicios.”<br> <strong>Dr. Miguel Ángel Mancera Espinosa, <span>Jefe de Gobierno</span></strong></p>
