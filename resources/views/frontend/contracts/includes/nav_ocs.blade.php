9b86f0966294b0eee491f7cf54de26ee2a5d8834
hugovom <hugo.om@gmail.com>
2016-06-14 19:38:30 -0500

    update nav

--- resources/views/frontend/contracts/includes/nav_ocs.blade.php
+++ resources/views/frontend/contracts/includes/nav_ocs.blade.php
@@ -1,8 +1,18 @@
 <nav>
 	<ul class="timeline">
+		
 		@if($elcontrato->singlecontracts->count())
-		<li><a href="#"  id="btn-contract-nav" class="nav_stage current" data-id="contracts" data-title="Contratación">
+		 	@foreach($elcontrato->singlecontracts as $c)
+				@if($c->implementation)
+				<?php $implementation = 1;?>
+					<li><a href="#" id="btn-implementation-nav" class="nav_stage current" data-id="implementation" data-title="Implementación">
+					<?php echo file_get_contents("img/nav_implementacion.svg"); ?></a>
+					</li>
+				@endif 
+			@endforeach
+		<li><a href="#"  id="btn-contract-nav" class="nav_stage {{ !$implementation ? 'current' : ''}}" data-id="contracts" data-title="Contratación">
 			<?php echo file_get_contents("img/nav_contratacion.svg"); ?></a>
+			<?php /**
 			<ul id="nav_contract">
 			  @foreach($elcontrato->singlecontracts as $contract)
 			  <?php $date_signed = strtotime($contract->date_signed);
@@ -11,13 +21,14 @@
 					<a  href="#" data-id="contract-{{$contract->id}}" data-title="{{ $contract->date_signed ? $date_signed : ''}}" class="t_right"></a>
 				</li>
 			  @endforeach
-			</ul>
+			</ul>***/?>
 		</li>
 	  	@endif
 
 		@if($elcontrato->awards->count())
 	  	<li><a href="#" id="btn-award-nav" class="nav_stage {{ !$elcontrato->singlecontracts ? 'current' : '' }}" data-id="awards" data-title="Adjudicación">
 	  		<?php echo file_get_contents("img/nav_adjudicacion.svg"); ?></a> 
+	  		<?php /**
 	  		<ul id="nav_award" {!! $elcontrato->singlecontracts->count() ? "class='hide'" : '' !!}>
 	  		  @foreach($elcontrato->awards as $award)
 	  		  <?php 
@@ -25,15 +36,15 @@
 	  		      $time_award = date('d/m/Y',$time_award);?>
 	  		  <li><a data-id="award-{{$award->id}}" href="#" data-title="{{ $award->date ? $time_award : ''}}" class="t_right"></a></li>
 	  		  @endforeach
-	  		</ul>
+	  		</ul>***/?>
 	  	</li>
 	  	@endif
 	  	
 	  	@if($elcontrato->tender)
-	  	<li {!! ($elcontrato->singlecontracts->count() || $elcontrato->awards->count()) ? '' : "class='active'"!!}><a href="#" data-id="tender" class="nav_stage {{ ($elcontrato->singlecontracts->count() || $elcontrato->awards->count()) ? '' : 'current'}}" data-title="Licitación"><?php echo file_get_contents("img/nav_licitacion.svg"); ?></a></li>
+	  	<li {!! ($elcontrato->singlecontracts->count() || $elcontrato->awards->count()) ? '' : "class='active'"!!}><a href="#" id="btn-tender-nav" data-id="tender" class="nav_stage {{ ($elcontrato->singlecontracts->count() || $elcontrato->awards->count()) ? '' : 'current'}}" data-title="Licitación"><?php echo file_get_contents("img/nav_licitacion.svg"); ?></a></li>
 	  	@endif
 	  	@if($elcontrato->planning)
-	  	<li><a href="#" data-id="planning"class="nav_stage" data-title="Planeación"><?php echo file_get_contents("img/nav_planeacion.svg"); ?></a></li>
+	  	<li><a href="#" id="btn-planning-nav" data-id="planning"class="nav_stage" data-title="Planeación"><?php echo file_get_contents("img/nav_planeacion.svg"); ?></a></li>
 	  	@endif
 	</ul>
 </nav>
