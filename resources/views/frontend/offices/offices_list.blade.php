9ebf9dfe4335629574f0051e5de07c1cffe90d71
hugovom <hugo.om@gmail.com>
2016-06-17 01:39:52 -0500

    update offices list

--- resources/views/frontend/offices/offices_list.blade.php
+++ resources/views/frontend/offices/offices_list.blade.php
@@ -12,16 +12,38 @@
 		</div>
 		<div class="col-sm-12">
 			<ul class="list">
+				<li>
+					<div class="col-sm-4">
+						<p>Dependencia</p>
+					</div>
+					<div class="col-sm-4">
+						<p>Dirección</p>
+					</div>
+					<div class="col-sm-2">
+						<p>Contacto</p>
+					</div>
+				</li>
 			@foreach ($buyers as $buyer)
 			<li>
-				<div class="col-sm-6 col-sm-offset-3">
+				<div class="col-sm-4">
 					<h3><a href="{{ url('dependencia/' . $buyer->id) }}">{{$buyer->name}}</a></h3>
 					<p>{{$buyer->address}} </p>
 				</div>
-				
+				<div class="col-sm-4">
+					<p>Dr. Lavista 144, <br>Delegación Cuauhtémoc</p>
+					<p>Ciudad de México C.P.06720</p>
+				</div>
+				<div class="col-sm-2">
+					<p>Tel.: 5588 3388</p>
+					<p><a href="http://www.finanzas.df.gob.mx">web</a></p>
+				</div>
+				<div class="col-sm-2">
+					<a href="{{ url('dependencia/' . $buyer->id) }}" class="btn">Contrataciones</a>
+				</div>
 				<div class="clearfix"></div>
 			</li>
 			@endforeach
+			<li></li>
 			</ul>
 		</div>
 		
