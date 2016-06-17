2bb65af41c7ddfd2e3859dd9b02b33c6813386de
hugovom <hugo.om@gmail.com>
2016-06-17 18:18:27 -0500

    update offices list

--- resources/views/frontend/offices/offices_list.blade.php
+++ resources/views/frontend/offices/offices_list.blade.php
@@ -5,49 +5,39 @@
 <div class="container">
 	<div class="row list">
 		<div class="col-sm-12">
-			<div class="box">
-				<h1 id="publisher-name">Lista de Dependencias con Contrataciones Abiertas</h1>
-				<div class="divider"></div>
+			<div class="row box">
+				<h1 class="mega_title">Lista de Dependencias con Contrataciones Abiertas</h1>
+				<div class="col-sm-10 col-sm-offset-1">
+					<div class="row guia">
+						<div class="col-sm-4"><p>Dependencia</p></div>
+						<div class="col-sm-3"><p>Dirección</p></div>
+						<div class="col-sm-2"><p>Contacto</p></div>
+					</div>
+					<ul class="list">
+					@foreach ($buyers as $buyer)
+						<li class="row">
+							<div class="col-sm-4">
+								<h3><a href="{{ url('dependencia/' . $buyer->id) }}">{{$buyer->name}}</a></h3>
+								<p>{{$buyer->address}} </p>
+							</div>
+							<div class="col-sm-3">
+								<p>Dr. Lavista 144, <br>Delegación Cuauhtémoc</p>
+								<p>Ciudad de México C.P.06720</p>
+							</div>
+							<div class="col-sm-2">
+								<p>Tel.: 5588 3388</p>
+								<p><a href="http://www.finanzas.df.gob.mx">web</a></p>
+							</div>
+							<div class="col-sm-3">
+								<a href="{{ url('dependencia/' . $buyer->id) }}" class="btn">Ver Dependencia</a>
+							</div>
+						</li>
+					@endforeach
+					</ul>
+					
+				</div>
 			</div>
 		</div>
-		<div class="col-sm-12">
-			<ul class="list">
-				<li>
-					<div class="col-sm-4">
-						<p>Dependencia</p>
-					</div>
-					<div class="col-sm-4">
-						<p>Dirección</p>
-					</div>
-					<div class="col-sm-2">
-						<p>Contacto</p>
-					</div>
-				</li>
-			@foreach ($buyers as $buyer)
-			<li>
-				<div class="col-sm-4">
-					<h3><a href="{{ url('dependencia/' . $buyer->id) }}">{{$buyer->name}}</a></h3>
-					<p>{{$buyer->address}} </p>
-				</div>
-				<div class="col-sm-4">
-					<p>Dr. Lavista 144, <br>Delegación Cuauhtémoc</p>
-					<p>Ciudad de México C.P.06720</p>
-				</div>
-				<div class="col-sm-2">
-					<p>Tel.: 5588 3388</p>
-					<p><a href="http://www.finanzas.df.gob.mx">web</a></p>
-				</div>
-				<div class="col-sm-2">
-					<a href="{{ url('dependencia/' . $buyer->id) }}" class="btn">Contrataciones</a>
-				</div>
-				<div class="clearfix"></div>
-			</li>
-			@endforeach
-			<li></li>
-			</ul>
-		</div>
-		
-		
 	</div>
 </div>
 @endsection
