034824dcf2e3a8888ef34515c1256d4d959a72ef
hugovom <hugo.om@gmail.com>
2016-06-18 13:36:46 -0500

    update contract view

--- resources/views/frontend/contracts/includes/nav_ocs.blade.php
+++ resources/views/frontend/contracts/includes/nav_ocs.blade.php
@@ -1,42 +1,22 @@
 <nav>
 	<ul class="timeline">
-		
 		@if($elcontrato->singlecontracts->count())
 		 	@foreach($elcontrato->singlecontracts as $c)
 				@if($c->implementation->transactions->count())
 				<?php $implementation = 1;?>
-					<li><a href="#" id="btn-implementation-nav" class="nav_stage current" data-id="implementation" data-title="Implementación">
+					<li class="active"><a href="#" id="btn-implementation-nav" class="nav_stage current" data-id="implementation" data-title="Implementación">
 					<?php echo file_get_contents("img/nav_implementacion.svg"); ?></a>
 					</li>
 				@endif 
 			@endforeach
-		<li><a href="#"  id="btn-contract-nav" class="nav_stage {{ !empty($implementation) ?  '' : 'current'}}" data-id="contracts" data-title="Contratación">
+			<li {!! !empty($implementation) ? '' : 'class="active"'  !!}><a href="#"  id="btn-contract-nav" class="nav_stage {{ !empty($implementation) ?  '' : 'current'}}" data-id="contracts" data-title="Contratación">
 			<?php echo file_get_contents("img/nav_contratacion.svg"); ?></a>
-			<?php /**
-			<ul id="nav_contract">
-			  @foreach($elcontrato->singlecontracts as $contract)
-			  <?php $date_signed = strtotime($contract->date_signed);
-				  	$date_signed = date('d/m/Y',$date_signed);?>
-				<li class="active">
-					<a  href="#" data-id="contract-{{$contract->id}}" data-title="{{ $contract->date_signed ? $date_signed : ''}}" class="t_right"></a>
-				</li>
-			  @endforeach
-			</ul>***/?>
-		</li>
+			</li>
 	  	@endif
 
 		@if($elcontrato->awards->count())
 	  	<li><a href="#" id="btn-award-nav" class="nav_stage {{ !$elcontrato->singlecontracts ? 'current' : '' }}" data-id="awards" data-title="Adjudicación">
 	  		<?php echo file_get_contents("img/nav_adjudicacion.svg"); ?></a> 
-	  		<?php /**
-	  		<ul id="nav_award" {!! $elcontrato->singlecontracts->count() ? "class='hide'" : '' !!}>
-	  		  @foreach($elcontrato->awards as $award)
-	  		  <?php 
-	  		      $time_award = strtotime($award->date);
-	  		      $time_award = date('d/m/Y',$time_award);?>
-	  		  <li><a data-id="award-{{$award->id}}" href="#" data-title="{{ $award->date ? $time_award : ''}}" class="t_right"></a></li>
-	  		  @endforeach
-	  		</ul>***/?>
 	  	</li>
 	  	@endif
 	  	
