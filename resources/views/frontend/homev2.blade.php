c75125b123765ad1327d2c72e671bff371a5ba5f
hugovom <hugo.om@gmail.com>
2016-06-18 12:12:01 -0500

    update home

--- resources/views/frontend/homev2.blade.php
+++ resources/views/frontend/homev2.blade.php
@@ -6,13 +6,12 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 col-xs-12">
-				<h1><strong>¿Cómo compra <span class="mx">tu</span> gobierno?</strong></h1>
+				<h1><strong>¿Cómo compra <span class="mx">tu gobierno</span>?</strong></h1>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-sm-8 col-xs-12 col-sm-offset-2">
-				<p class="info center">La <strong>CD<span class="mx">MX</span></strong> 
-							es la primera ciudad en el mundo en publicar información sobre todo su proceso de contrataciones.*</p>
+				<p class="info center">La <strong>CD<span class="mx">MX</span></strong> es la primera ciudad en el mundo en publicar información sobre todo su proceso de contrataciones.<sup>1</sup></p>
 			
 				<div class="bill">
 					<div class="row">
@@ -21,13 +20,16 @@
 							<h3><span id="contracts">0</span></h3>
 						</div>
 						<div class="col-sm-5 col-xs-6 ">
-							<p>Monto Total Contratado (MXN)</p>
-							<h3>$<strong><span id="amount">0</span></strong><span class="mxn">Millones</span></h3>
+							<p>Monto Total Contratado (MXN)<sup>2</sup></p>
+							<h3>$<strong><span id="amount">0</span></strong><span class="mxn"> millones</span></h3>
 						</div>
 						
 					</div>
 				</div>
-				<p class="footnote center">*Información disponible a partir de la implementación del estándar en 2015.</p>
+				<div class="row">
+					<div class="col-sm-9 col-sm-offset-2">
+					<p class="footnote"><strong>1</strong> Información disponible a partir de la implementación del estándar en 2015.<br><strong>2</strong> El monto contratado corresponde a las contrataciones abiertas realizadas en pesos mexicanos, por lo que el total no contiene las compras  realizadas en otras monedas.</p>
+				</div>
 				<div class="row">
 					<div class="col-md-6 col-sm-12">
 						<a href="{{ url('contratos') }}" class="btn cta">Explora las compras de la <strong>CDMX &#x2794;</strong> </a>
@@ -45,6 +47,10 @@
 <section class="etapas">
 	<div class="container">
 		<div class="row">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h2 class="center">Proceso de Compra de la <strong>CD<span class="mx">MX</span></strong>.</h2>
+				<p class="info">Cada vez que <strong>tu gobierno realiza una compra</strong>, completa un proceso para elegir lo que más le convenga.</p>
+			</div>
 			<div class="col-sm-10 col-sm-offset-1">
 				<iframe src="https://www.youtube.com/embed/xQvKHiERIJI" frameborder="0" allowfullscreen></iframe>
 			</div>
