00240bef855966c4da12f84f4ffbdde8b05cd4cf
hugovom <hugo.om@gmail.com>
2016-06-18 12:25:48 -0500

    welcome back dear title

--- resources/views/frontend/layouts/nav.blade.php
+++ resources/views/frontend/layouts/nav.blade.php
@@ -9,10 +9,13 @@
 						<h2><a href="http://www.df.gob.mx/"  class="cdmx">CDMX</a></h2>
 					</div>
 					<div class="col-xs-6 col-sm-6">
+						<h1>
 						@if ($body_class == 'home2') 
+						CONTRATACIONES <strong>ABIERTAS</strong>
 						@else
-						<h1><a href="{{url('')}}">CONTRATACIONES <strong>ABIERTAS</strong></a></h1>
+						<a href="{{url('')}}">CONTRATACIONES <strong>ABIERTAS</strong></a>
 						@endif
+						</h1>
 					</div>
 					
 				</div>
