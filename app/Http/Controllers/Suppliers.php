ece251407ec2c26a55ff858915898024a221cc66
el coruco <el.coruco@gmail.com>
2016-07-03 09:10:31 -0500

    se documentó el controller de proveedores

--- app/Http/Controllers/Suppliers.php
+++ app/Http/Controllers/Suppliers.php
@@ -15,10 +15,24 @@ use App\Models\Provider;
 use App\Models\BuyerProvider;
 use App\Models\Buyer;
 
+/*
+ * El controller de proveedores. Aquí se muestra la lista de proveedores
+ * y cada proveedor por separado.
+ *
+ * funciones disponibles en el primer release: 
+ * - index
+ * - show
+*/
 class Suppliers extends Controller {
 	//
 	// Suppliers list
-	//	
+	//
+	// Regresa la lista de proveedores, ordenados por nombre. 
+	// Se accede a esta función mediante:
+	// proveedores
+  //
+	// nota: necesita paginación, y pasar el filtro que existe en el view al controller
+	//
 	public function index(){
 		$providers 			     = Provider::orderby('name','ASC')->get();
 		$data                = [];
@@ -35,30 +49,38 @@ class Suppliers extends Controller {
 	//
 	// Supplier 
 	//
+	// Muestra un proveedor mediante el rfc. 
+	// se accede a esta función mediante: 
+	// proveedor/{id}
+	//
+	// nota: es posible optimizar esta función utilizando ORM, sin la necesidad de 
+	// cargar todos los elementos y filtrarlos. 
+	//
 	public function show($rfc){
-		$tenderer 			 		= Provider::where("rfc", $rfc)->get()->first();							 		
-		$tender_tenderer 	 		= TenderTenderer::where("tenderer_id", $tenderer->id)->get();
-		$tenders			 		= Tender::all();
-		$supplier 			 		= Provider::where("rfc", $rfc)->get()->first();
-		$buyers				    	= BuyerProvider::where('provider_id',$supplier->id)->get();
+		$tenderer        = Provider::where("rfc", $rfc)->get()->first();							 		
+		$tender_tenderer = TenderTenderer::where("tenderer_id", $tenderer->id)->get();
+		$tenders         = Tender::all();
+		$supplier        = Provider::where("rfc", $rfc)->get()->first();
+		$buyers          = BuyerProvider::where('provider_id',$supplier->id)->get();
 				
-		$contracts 			 		= Contract::all();
-		$awards 			 		= Award::all();
-		$singlecontracts 	 		= SingleContract::all();
+		$contracts       = Contract::all();
+		$awards          = Award::all();
+		$singlecontracts = SingleContract::all();
+		
 		$data                		= [];
 		$data['title']       		= $tenderer->name;
 		$data['description'] 		= 'Proveedor';
-		$data['og_image']	 		= "img/og/contrato-cdmx.png";
+		$data['og_image']	      = "img/og/contrato-cdmx.png";
 		$data['body_class']  		= 'proveedor';
 		
-		$data['contracts']  		= $contracts;
-		$data['tenderer']   		= $tenderer;
-		$data['supplier']   		= $supplier;
-		$data['sicon']  			= $singlecontracts;
-		$data['awards']   			= $awards;
-		$data['tender_tenderer']   	= $tender_tenderer;
-		$data['tenders']   			= $tenders;
-		$data['buyers']   			= $buyers;
+		$data['contracts']  		 = $contracts;
+		$data['tenderer']   		 = $tenderer;
+		$data['supplier']   		 = $supplier;
+		$data['sicon']  			   = $singlecontracts;
+		$data['awards']   			 = $awards;
+		$data['tender_tenderer'] = $tender_tenderer;
+		$data['tenders']   			 = $tenders;
+		$data['buyers']   			 = $buyers;
 
 		
 		return view("frontend.suppliers.supplier")->with($data);
