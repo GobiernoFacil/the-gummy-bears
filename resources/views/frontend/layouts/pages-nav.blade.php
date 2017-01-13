2aa1a70e41ba0b2b7eec0b718e0652bbfb1c2945
hugovom <hugo.om@gmail.com>
2017-01-13 01:34:29 -0600

    fix #93

--- resources/views/frontend/layouts/pages-nav.blade.php
+++ resources/views/frontend/layouts/pages-nav.blade.php
@@ -6,7 +6,7 @@
 	<div class="pages-nav__item"><a class="link link--page" href="{{ url('proveedores') }}">Proveedores</a></div>
 	<div class="pages-nav__item"><a class="link link--page" href="{{ url('por-que') }}">¿Por qué las contrataciones abiertas?</a></div>
 	<div class="pages-nav__item"><a class="link link--page" href="{{ url('datos-abiertos') }}">Usar los datos</a></div>
-	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('glosario') }}">Términos Clave</a></div>
+	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('glosario') }}">Preguntas Frecuentes</a></div>
 	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('contacto') }}">Contacto</a></div>
 	<div class="pages-nav__item pages-nav__item--small"><a class="link link--page link--faded" href="{{ url('privacidad') }}">Privacidad &amp; Términos</a></div>
 </nav>
