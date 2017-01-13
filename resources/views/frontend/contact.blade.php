9644133a2a4c1c93b9abffce1a8021f4f016745b
hugovom <hugo.om@gmail.com>
2017-01-13 01:32:39 -0600

    fix #87

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -15,8 +15,12 @@
 				<div class="codigo">
 					<div class="row">
 						<div class="col-sm-10 col-sm-offset-1">
-							<p>En caso de opiniones o comentarios, favor de enviar un correo a  <a href="mailto:contratacionesabiertas@cdmx.gob.mx">contratacionesabiertas@cdmx.gob.mx</a>
-							</p>
+							<p>En caso de opiniones o comentarios, favor de enviar a la dependencia correspondiente en los siguientes correos electrónicos:</p>		
+							<ul>
+								<li>Secretaría de Finanzas: <a href="maito:finanzas_contratosabiertos@cdmx.gob.mx">finanzas_contratosabiertos@cdmx.gob.mx</a></li>
+								<li>Secretaría de Obras y Servicios: <a href="obras_contratosabiertos@cdmx.gob.mx">obras_contratosabiertos@cdmx.gob.mx</a></li>
+								<li>Oficialía Mayor: <a href="oficialia_contratosabiertos@cdmx.gob.mx">oficialia_contratosabiertos@cdmx.gob.mx</a></li>
+							</ul>
 						</div>
 					</div>
 				</div>
